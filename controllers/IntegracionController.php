424f9dea9b5306206af1c3a1bb93a5ef8d89f718
Daniel Chirinos <daniel.chirinos@bermann.cl>
2023-09-14 13:35:15 -0300

    .

--- controllers/IntegracionController.php
+++ controllers/IntegracionController.php
@@ -2731,13 +2731,13 @@ class IntegracionController extends Controller{
             header('Access-Control-Allow-Methods: GET, OPTIONS');
             header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization, Autorizacion");
             header('Content-Type: application/json');
-            // $method = $_SERVER['REQUEST_METHOD'];
-            // if ($method == "OPTIONS") {
-            //     header('Access-Control-Allow-Origin: *');
-            //     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization, Autorizacion");
-            //     header("HTTP/1.1 200 OK");
-            //     die();
-            // }
+            $method = $_SERVER['REQUEST_METHOD'];
+            if ($method == "OPTIONS") {
+                header('Access-Control-Allow-Origin: *');
+                header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Autorizacion");
+                header("HTTP/1.1 200 OK");
+                die();
+            }
         }
 
         
